b07cfb14d1ed6296ac64cdd53dee71daf8a376c6
ComanderIke <keppidani@gmail.com>
2021-01-22 21:34:42 +0100

    added some CharacterUI

---
+++ trunk/FireEmblemMobile/FireEmblemMobileUnityProject/Assets/UICameraEventMaskScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICameraEventMaskScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().eventMask = LayerMask.GetMask("UI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
