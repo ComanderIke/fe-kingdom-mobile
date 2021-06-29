9f85e584293c8c9cb5eec11103048bf2c53bbfe2
keppi <keppidani@gmail.com>
2021-06-29 13:40:14 +0200

    added tavern and barracks screen

--- trunk/FireEmblemMobile/FireEmblemMobileUnityProject/Assets/- 2 - Scripts/Game/Utility/TweenableUIController.cs
+++ trunk/FireEmblemMobile/FireEmblemMobileUnityProject/Assets/- 2 - Scripts/Game/Utility/TweenableUIController.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Game.Utility
 {
@@ -6,17 +8,34 @@ namespace Game.Utility
     {
         // Start is called before the first frame update
         public RectTransform tweenObj;
+        public UnityEvent AfterHide;
+        public CanvasGroup CanvasGroup;
 
         public void Hide()
         {
-            
-            LeanTween.scale(tweenObj, Vector3.zero, 0.8f).setDelay(1.0f).setEaseInOutQuad().setOnComplete(()=>gameObject.SetActive(false));
+
+            LeanTween.scale(tweenObj, Vector3.zero, 0.4f).setDelay(0.0f).setEaseInQuad()
+                .setOnComplete(() =>
+                {
+                    gameObject.SetActive(false);
+                    AfterHide?.Invoke();
+                    
+                });
+            LeanTween.alphaCanvas(CanvasGroup, 0, 0.4f).setDelay(0.0f).setEaseInQuad()
+                .setOnComplete(() =>
+                {
+                    // gameObject.SetActive(false);
+                    // AfterHide?.Invoke();
+                    
+                });
         }
         public void Show()
         {
             gameObject.SetActive(true);
             tweenObj.transform.localScale = Vector3.zero;
-            LeanTween.scale(tweenObj, Vector3.one, 0.8f).setDelay(1.0f).setEaseInOutQuad();
+            LeanTween.scale(tweenObj, Vector3.one, 0.6f).setDelay(1.0f).setEase( LeanTweenType.easeOutBack ).setOvershoot(0.5f);//.setPeriod(0.1f);
+            CanvasGroup.alpha = 0;
+            LeanTween.alphaCanvas(CanvasGroup, 1, 0.6f).setDelay(1.0f).setEaseInQuad();
         }
     }
 }
