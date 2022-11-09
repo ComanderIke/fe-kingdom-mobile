f9df10488d2eea33b3671242d86eef5f65ff5a70
Daniel Kepplinger <kepplinger.daniel@gmail.com>
2022-11-09 19:11:10 +0100

    added savefile backups

--- trunk/FireEmblemMobile/FireEmblemMobileUnityProject/Assets/02_Scripts/Game/SerializedData/SaveGameManager.cs
+++ trunk/FireEmblemMobile/FireEmblemMobileUnityProject/Assets/02_Scripts/Game/SerializedData/SaveGameManager.cs
@@ -21,6 +21,7 @@ namespace LostGrace
     {
         public const int SaveFileCount = 3;
         private const string SaveFileName = "save";
+        private const string backupExtension = ".bak";
         public static SaveData currentSaveData;
         private static List<IDataPersistance> dataPersistanceObjects= new List<IDataPersistance>();
 
@@ -51,32 +52,74 @@ namespace LostGrace
         }
         public static void Save(int slotNumber)
         {
-            
-            foreach (IDataPersistance dataPersistance in dataPersistanceObjects)
+            try
             {
-                dataPersistance.SaveData(ref currentSaveData);
-            }
-            
-            var formatter = new BinaryFormatter();
-            string pathFolder = Application.persistentDataPath + "/saves/";
-            if (!Directory.Exists(pathFolder))
-            {
-                Directory.CreateDirectory(pathFolder);
-            }
+                foreach (IDataPersistance dataPersistance in dataPersistanceObjects)
+                {
+                    dataPersistance.SaveData(ref currentSaveData);
+                }
 
-            currentSaveData.saveSlot = slotNumber;
-            string path =  Path.Combine(pathFolder , SaveFileName+slotNumber+".fe");
-            var jsonData=JsonUtility.ToJson(currentSaveData, true);
-            Debug.Log("Save Game: " + path);
-            if (WriteToFile(path, jsonData))
+                var formatter = new BinaryFormatter();
+                string pathFolder = Application.persistentDataPath + "/saves/";
+                if (!Directory.Exists(pathFolder))
+                {
+                    Directory.CreateDirectory(pathFolder);
+                }
+
+                currentSaveData.saveSlot = slotNumber;
+                string path = Path.Combine(pathFolder, SaveFileName + slotNumber + ".fe");
+                string backupPath = path + backupExtension;
+                var jsonData = JsonUtility.ToJson(currentSaveData, true);
+                Debug.Log("Save Game: " + path);
+                if (WriteToFile(path, jsonData))
+                {
+                    Debug.Log("Save Successfull!");
+                }
+
+                Load(slotNumber);
+                if (currentSaveData != null)
+                {
+                    File.Copy(path, backupPath, true);
+                }
+                else
+                {
+                    Debug.LogError("Save File could not be verified and backup could not be created");
+                }
+            }
+            catch (Exception e)
             {
-                Debug.Log("Save Successfull!");
+                Debug.LogError("File could not be saved: Slot: "+slotNumber+"\n"+e);
             }
             // var stream = new FileStream(path, FileMode.Create);
-          
-            
+
+
             //formatter.Serialize(stream, data);
             //stream.Close();
+        }
+
+        private static bool AttemptRollback(string fullPath)
+        {
+            bool success = false;
+            string backUpFilePath = fullPath + backupExtension;
+            try
+            {
+                if (File.Exists(backUpFilePath))
+                {
+                    File.Copy(backUpFilePath, fullPath, true);
+                    success = true;
+                    Debug.LogWarning("had to roll back to backup file at: "+backUpFilePath);
+                }
+                else
+                {
+                    Debug.LogError("Tried to rollback but no backup file exists at: "+backupExtension+"\n");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error occured when trying to roll back to backup file at: "+backupExtension+"\n"+e);
+            }
+            
+            return success;
         }
         private static bool WriteToFile(string path, string data)
         {
@@ -107,39 +150,61 @@ namespace LostGrace
 
             return false;
         }
-        public static void Load(int slot)
+        public static void Load(int slot, bool allowRollback=true)
         {
             string path = Path.Combine(Application.persistentDataPath+"/saves", SaveFileName+slot+".fe");
-            if (File.Exists(path))
+            try
             {
-                // var formatter = new BinaryFormatter();
-                // var stream =  new FileStream(path, FileMode.Open);
-                // var data = formatter.Deserialize(stream); //as Saveata;
-                // stream.Close();
-               
-                Debug.Log("Load Data from: "+path);
-                var json = "";
-                if (LoadFromFile(path, out json))
+
+
+                if (File.Exists(path))
                 {
-                    Debug.Log("Loading Successfull!");
+                    // var formatter = new BinaryFormatter();
+                    // var stream =  new FileStream(path, FileMode.Open);
+                    // var data = formatter.Deserialize(stream); //as Saveata;
+                    // stream.Close();
+
+                    Debug.Log("Load Data from: " + path);
+                    var json = "";
+                    if (LoadFromFile(path, out json))
+                    {
+                        Debug.Log("Loading Successfull!");
+                    }
+
+                    currentSaveData = new SaveData(0, "tmp");
+                    JsonUtility.FromJsonOverwrite(json, currentSaveData);
+                    Debug.Log("FileSlotNameBer: " + currentSaveData.fileLabel);
+                    if (currentSaveData == null)
+                    {
+                        throw new Exception("Data is null!");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Save File not found in " + path);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning("Failed to load data file. Attempting to roll back");
+                if (allowRollback)
+                {
+                    bool rollbackSuccess = AttemptRollback(path);
+                    if (rollbackSuccess)
+                    {
+                        Load(slot, false);
+                    }
                 }
 
-                currentSaveData = new SaveData(0,"tmp");
-                JsonUtility.FromJsonOverwrite(json, currentSaveData);
-                Debug.Log("FileSlotNameBer: "+currentSaveData.fileLabel);
-                
-            }
-            else
-            {
-                Debug.LogError("Save File not found in " +path);
-                
             }
 
-            if (currentSaveData == null)
-            {
-                Debug.LogError("No data was found. Initializing new game data.");
-                NewGame(slot, "File "+slot);
-            }
+
+            // if (currentSaveData == null)
+            // {
+            //     Debug.LogError("No data was found. Initializing new game data.");
+            //     NewGame(slot, "File "+slot);
+            // }
 
             foreach (IDataPersistance dataPersistance in dataPersistanceObjects)
             {
