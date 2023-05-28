b6036244ab6dd54b4decc45b647b595341b52a61
Daniel Kepplinger <kepplinger.daniel@gmail.com>
2023-05-28 10:44:35 +0200

    Added FadeInandOut To MvP Panel
    And made it close on click after a minimum showTime

--- trunk/FireEmblemMobile/FireEmblemMobileUnityProject/Assets/02_Scripts/Game/GUI/ResultMVPPanelUI.cs
+++ trunk/FireEmblemMobile/FireEmblemMobileUnityProject/Assets/02_Scripts/Game/GUI/ResultMVPPanelUI.cs
@@ -1,19 +1,48 @@
-﻿using Game.GameActors.Units;
+﻿using System;
+using Game.GameActors.Units;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LostGrace
 {
     public class ResultMVPPanelUI : MonoBehaviour
     {
-        [SerializeField] private Animator treasureChest;
-        [SerializeField] private TextMeshProUGUI mvpName;
-        [SerializeField] private Animator mvpIdleAnimation;
 
-        public void Show(Unit mvp, GachaReward getGachaReward)
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private TextMeshProUGUI mvpName;
+        [SerializeField] private Image mvpFaceSprite;
+        [SerializeField] private float minTimeShown = 1.0f;
+        private bool shown = false;
+        private float time = 0;
+
+        public void Show(Unit mvp)
         {
+            gameObject.SetActive(true);
             mvpName.text = mvp.Name;
-            mvpIdleAnimation.runtimeAnimatorController = mvp.visuals.Prefabs.UIAnimatorController;
+            mvpFaceSprite.sprite= mvp.visuals.CharacterSpriteSet.FaceSprite;
+            TweenUtility.FadeIn(canvasGroup);
+            shown = true;
+            time = 0;
+        }
+
+        private void Update()
+        {
+            if (shown)
+            {
+                time += Time.deltaTime;
+                if (time >= minTimeShown)
+                {
+                    if(Input.GetMouseButtonDown(0))
+                        Hide();
+                }
+            }
+        }
+
+        public void Hide()
+        {
+            shown = false;
+            TweenUtility.FadeOut(canvasGroup).setOnComplete(() => gameObject.SetActive(false));
         }
     }
 }
