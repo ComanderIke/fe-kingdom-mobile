5a217a36262b3276905895665fd1f9e4f929242a
Daniel from Mac <keppidani@gmail.com>
2024-01-14 17:20:08 +0100

    added error console

---
+++ trunk/FireEmblemMobile/FireEmblemMobileUnityProject/Assets/ErrorScreenUI.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace LostGrace
+{
+    public class ErrorScreenUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI errorText;
+        // Start is called before the first frame update
+        void Awake()
+        {
+            Application.logMessageReceived -= LogMessageReceived;
+            Application.logMessageReceived += LogMessageReceived;
+            Hide();
+        }
+
+        void LogMessageReceived(string condition, string stackTrace, LogType logType)
+        {
+            if (logType == LogType.Exception || logType == LogType.Error)
+            {
+                Show();
+                errorText.text = "Error: " + condition + "\n" + stackTrace;
+            }
+        }
+
+        void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            Application.logMessageReceived -= LogMessageReceived;
+        }
+    }
+}
